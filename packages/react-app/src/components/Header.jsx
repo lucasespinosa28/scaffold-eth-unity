a3b067487f8f43c8a732994fd11f5106ef962964
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2021-09-06 04:00:51 -0300

    title

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="🏗 crabbytoken"
+        subTitle="The crabby token app that shows tokens in unity"
         style={{ cursor: "pointer" }}
       />
     </a>
